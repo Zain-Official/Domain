b63cbae1c8eb64551c753ef03eb713ccab36f655
Zain <zainofficial03@gmail.com>
2021-05-30 12:28:47 +0500

    Add project files.

---
+++ Domain/Controllers/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Domain.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+       public string Name(string name)
+        {
+            
+            return "Wel Come to "+name;
+        }
+
+        public string FullName(string fname =null,string lname =null)
+        {
+            string F_NAME = string.Empty;
+            if(fname !=null && lname !=null)
+            {
+                F_NAME = "Your First Name Is : " + fname + " And Last Name Is : " + lname;
+            }
+            else if (fname !=null && lname == null)
+            {
+                F_NAME = "Your First Name Is : " + fname;
+            }
+            else if (fname ==null && lname !=null)
+            {
+                F_NAME = "Your Last Name Is : " + lname;
+            }
+            else if(fname ==null && lname ==null)
+            {
+                F_NAME = "Please Enter Your Name ";
+            }
+
+
+            return F_NAME;
+
+        }
+    }
+}
---
+++ Domain/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Domain.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+     public string Name(int id,string nam)
+        {
+            return "Name : "+nam+" ID : "+id;
+        }
+    }
+}
